21a8071655430eeb2a39d78ef0fd651a4ae15593
KuntalRathod <nature.kr70@gmail.com>
2024-05-08 14:49:25 +0530

    Add .env.sample and update Firebase configuration

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDVrBzyhvaFPC0knuenWcUMuJvCdH7GSnA",
-  authDomain: "clone-7cacf.firebaseapp.com",
-  projectId: "clone-7cacf",
-  storageBucket: "clone-7cacf.appspot.com",
-  messagingSenderId: "568934899724",
-  appId: "1:568934899724:web:1e3dbaa99497a88ebf0e29",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 }
 
 // Initialize Firebase
